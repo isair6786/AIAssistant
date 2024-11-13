0629446eb3fc1c7f3c40d784c85b05857c3b1c62
Kevin Mendoza <64709883+isair6786@users.noreply.github.com>
2024-11-12 22:08:57 -0600

    añadiendo timeout

--- AisyAssistant/Chat/Functions/Providers/apiFunctions.py
+++ AisyAssistant/Chat/Functions/Providers/apiFunctions.py
@@ -21,30 +21,36 @@ def api_enviar_correo(payload):
         return False
 
 def api_leer_eventos(uid):
-
     try:
-       # Obtener la fecha actual
+        # Obtener la fecha actual
         fecha_actual = datetime.now().date()
         print("La fecha actual es:", fecha_actual)
-        URL = os.getenv('URL_API')+"/api/pythonGetCalendarEventsbyDate" + f"?uid={uid}&date={fecha_actual}&endDate={fecha_actual}"  # Asegúrate de que la URL esté correctamente configurada
+        URL = os.getenv('URL_API') + "/api/pythonGetCalendarEventsbyDate" + f"?uid={uid}&date={fecha_actual}&endDate={fecha_actual}"  # Asegúrate de que la URL esté correctamente configurada
 
-        # Realizar la solicitud get
-        response = requests.get(URL)  # 'json' serializa automáticamente el dict en JSON
-       
-        return(response.text)
+        # Realizar la solicitud get con un timeout de 180 segundos (3 minutos)
+        response = requests.get(URL, timeout=(30,90))  # Agregamos el timeout
+
+        # Devuelve el contenido de la respuesta si es exitosa
+        return response.text
+    except requests.Timeout:
+        print("Error al consultar los correos, el servicio no esta disponible por el momento , intente nuevamente ")
+        return "Error al consultar los correos, el servicio no esta disponible por el momento , intente nuevamente "
     except requests.RequestException as e:
         print(f"Excepción al enviar la solicitud: {e}")
-        return False
+        return f"Error al realizar la solicitud: {e}"
 def api_leer_correos(uid):
 
     try:
         URL = os.getenv('URL_API')+f"/api/pythonReadEmailbyProvider?uid={uid}"
         # Realizar la solicitud get
-        response = requests.get(URL)  # 'json' serializa automáticamente el dict en JSON
+        response = requests.get(URL,timeout=(30,90))  # 'json' serializa automáticamente el dict en JSON
         return(response.text)
+    except requests.Timeout:
+        print("Error al consultar los correos, el servicio no esta disponible por el momento , intente nuevamente ")
+        return "Error al consultar los correos, el servicio no esta disponible por el momento , intente nuevamente "
     except requests.RequestException as e:
         print(f"Excepción al enviar la solicitud: {e}")
-        return False
+        return f"Error al realizar la solicitud: {e}"
 
 def api_agendar_evento(attendees, descripcion_evento, end, isAllDay, start, titulo, uid, correoUid):
     try:
